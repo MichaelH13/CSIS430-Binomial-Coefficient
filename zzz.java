66869b8afd9d4efb00cc401a5cd342dfe50c13a0
MichaelH13 <mhiggins13@georgefox.edu>
2016-02-27 22:17:04 -0800

    Initial Commit

---
+++ zzz.java
@@ -0,0 +1,18 @@
+public class ZZZ {
+   
+   public static long nChooseKRecursive(int n, int k) {
+   
+   }
+   
+   public static long nChooseKDynamic(int n, int k) {
+   
+   }
+   
+   public static long nChooseK(int n, int k) {
+   
+   }
+   
+   private static long factorial(int n) {
+   
+   }
+}
